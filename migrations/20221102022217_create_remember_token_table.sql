6a2efb95415991c75de896d5d01a4bccef0115b4
Evgenii Elchev <yolchev@gmail.com>
2022-11-11 14:57:21 +0700

    change tipes to common

--- migrations/20221102022217_create_remember_token_table.sql
+++ migrations/20221102022217_create_remember_token_table.sql
@@ -1,6 +1,6 @@
 -- Add migration script here
 CREATE TABLE remember_tokens (
    user_id TEXT NOT NULL,
-   token VARCHAR(255) NOT NULL,
+   token TEXT NOT NULL,
    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
--- migrations/20221103103601_create_movie_table.sql
+++ migrations/20221103103601_create_movie_table.sql
@@ -3,8 +3,8 @@ CREATE TABLE movies(
    id INTEGER PRIMARY KEY NOT NULL,
    title TEXT NOT NULL,
    director TEXT NOT NULL,
-   relise_date SMALLINT NOT NULL,
-   rating TINYINT NOT NULL,
+   relise_date INTEGER NOT NULL,
+   rating INTEGER NOT NULL,
    poster_id TEXT NOT NULL,
    created_at DATETIME NOT NULL
 );
